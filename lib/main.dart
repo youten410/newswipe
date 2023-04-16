d3b50fb0038ce035378da043907c8ff5d295a628
Shun Sato <techregisteremail@gmail.com>
2023-04-17 01:24:44 +0900

    update: get Data Using News API

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Swipe Cards Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: NewsApp(),
     );
@@ -21,18 +20,44 @@ class MyApp extends StatelessWidget {
 }
 
 class NewsApp extends StatefulWidget {
-  const NewsApp({super.key});
+  const NewsApp({Key? key}) : super(key: key);
 
   @override
   State<NewsApp> createState() => _NewsAppState();
 }
 
 class _NewsAppState extends State<NewsApp> {
+  List items = [];
+  List imageList = [];
+  String status = '';
+
+  var url = 'https://newsapi.org/v2/everything?' +
+      'q=Apple&' +
+      'from=2023-04-16&' +
+      'sortBy=popularity&' +
+      'apiKey=d29107383eac4c97989831bb265caaaa';
+
+  Future<void> getData() async {
+    var response = await Dio().get(url);
+    status = response.data['status'];
+    imageList = response.data['articles'];
+    setState(() {});
+    print(status);
+  }
+
   @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('NewsApp'),
+        title: Text('News App'),
+      ),
+      body: Center(
+        child: Text('Status: $status'),
       ),
     );
   }
