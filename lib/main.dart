1729d52cefd8e20e5dff01f973db195406d2f916
Shun Sato <techregisteremail@gmail.com>
2023-04-19 01:58:28 +0900

    update: カテゴリボタンを追加しました。

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -30,13 +31,19 @@ class NewsApp extends StatefulWidget {
 class _NewsAppState extends State<NewsApp> {
   List items = [];
   String status = '';
-  List menuItem = ['フランス','米国','英国','ドイツ','日本','イタリア','カナダ'];
+  List<String> menuItem = ['フランス', '米国', '英国', 'ドイツ', '日本', 'イタリア', 'カナダ'];
   String country = 'jp';
   int countryIndex = 4;
+
+  List<String> categoryList = ['経済', 'エンタメ', 'ヘルス','科学','スポーツ','テクノロジー'];
+  String category = 'business';
+  int categoryIndex = 0;
+  
   String url = '';
 
-  Future<void> getData(country) async {
-    var url = 'https://newsapi.org/v2/top-headlines?country=$country&apiKey=d29107383eac4c97989831bb265caaaa';
+  Future<void> getData(country,category) async {
+    var url =
+        'https://newsapi.org/v2/top-headlines?country=$country&category=$category&apiKey=d29107383eac4c97989831bb265caaaa';
     //print(url);
     var response = await Dio().get(url);
     status = response.data['status'];
@@ -46,14 +53,57 @@ class _NewsAppState extends State<NewsApp> {
   }
 
   Future<void> _refreshNews() async {
-    await getData(country);
+    await getData(country,category);
   }
 
   @override
   void initState() {
     super.initState();
-    url = 'https://newsapi.org/v2/top-headlines?country=$country&apiKey=d29107383eac4c97989831bb265caaaa';
-    getData(country);
+    url =
+        'https://newsapi.org/v2/top-headlines?country=$country&category=$category&apiKey=d29107383eac4c97989831bb265caaaa';
+    getData(country,category);
+  }
+
+  Widget buildCategoryList(BuildContext context, int index) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 1.0),
+      child: MenuItemButton(
+        onPressed: () {
+          print("${categoryList[index]}が選択されました");
+          switch (categoryList[index]){
+            case '経済':
+              category = 'business';
+              categoryIndex = 0;
+              break;
+            case 'エンタメ':
+              category = 'entertainment';
+              categoryIndex = 1;
+              break;
+            case 'ヘルス':
+              category = 'health';
+              categoryIndex = 2;
+              break;
+            case '科学':
+              category = 'science';
+              categoryIndex = 3;
+              break;
+            case 'スポーツ':
+              category = 'sports';
+              categoryIndex = 4;
+              break;
+            case 'テクノロジー':
+              category = 'technology';
+              categoryIndex = 5;
+              break;
+          }
+          getData(country,category);
+        },
+        child: Text(
+          categoryList[index],
+          style: const TextStyle(color: Colors.black),
+        ),
+      ),
+    );
   }
 
   Widget build(BuildContext context) {
@@ -101,11 +151,11 @@ class _NewsAppState extends State<NewsApp> {
                           countryIndex = 5;
                           break;
                         case 'カナダ':
-                         country = 'ca';
-                         countryIndex = 6;
-                         break;
+                          country = 'ca';
+                          countryIndex = 6;
+                          break;
                       }
-                      getData(country);
+                      getData(country,category);
                     },
                   ),
                   const Divider(
@@ -120,38 +170,52 @@ class _NewsAppState extends State<NewsApp> {
       appBar: AppBar(
         title: Text('${menuItem[countryIndex]}のニュース'),
       ),
-      body: RefreshIndicator(
-        onRefresh: _refreshNews,
-        child: ListView.separated(
-          itemCount: items.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Card(
-              child: Column(
-                children: <Widget>[
-                  ListTile(
-                    title: Text(items[index]['title'] ?? 'Unknown Title'),
-                    subtitle: Text(items[index]['author'] ?? 'Unknown Author'),
-                    trailing: Icon(Icons.arrow_forward),
-                    onTap: () async {
-                      final url =
-                          Uri.parse(items[index]['url'] ?? 'Unknown Title');
-                      // ignore: deprecated_member_use
-                      if (await canLaunch(url.toString())) {
-                        // ignore: deprecated_member_use
-                        await launch(url.toString());
-                      } else {
-                        print("Can't launch url");
-                      }
-                    },
-                  ),
-                ],
+      body: Column(
+        children: [
+          Container(
+            height: 50.0,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: categoryList.length,
+              itemBuilder: buildCategoryList,
+            ),
+          ),
+          Expanded(
+            child: RefreshIndicator(
+              onRefresh: _refreshNews,
+              child: ListView.separated(
+                itemCount: items.length,
+                separatorBuilder: (BuildContext context, int index) =>
+                    const Divider(),
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    child: Column(
+                      children: <Widget>[
+                        ListTile(
+                          title: Text(items[index]['title'] ?? 'Unknown Title'),
+                          subtitle:
+                              Text(items[index]['author'] ?? 'Unknown Author'),
+                          trailing: Icon(Icons.arrow_forward),
+                          onTap: () async {
+                            final url = Uri.parse(
+                                items[index]['url'] ?? 'Unknown Title');
+                            // ignore: deprecated_member_use
+                            if (await canLaunch(url.toString())) {
+                              // ignore: deprecated_member_use
+                              await launch(url.toString());
+                            } else {
+                              print("Can't launch url");
+                            }
+                          },
+                        ),
+                      ],
+                    ),
+                  );
+                },
               ),
-            );
-          },
-          separatorBuilder: (BuildContext context, int index) {
-            return Divider();
-          },
-        ),
+            ),
+          ),
+        ],
       ),
     );
   }
