422ebf3d34e707f9c723635c35772082c652dd08
Shun Sato <techregisteremail@gmail.com>
2023-05-30 19:00:31 +0900

    delete : グーグルサインインボタンのパッケージを削除

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:http/http.dart' as http;
 import 'package:news_app/news_list.dart';
 import 'package:provider/provider.dart';
