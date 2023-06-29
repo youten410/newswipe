e0608a67b2d34f0293b83e86a9b7ab825e34d4e7
Shun Sato <techregisteremail@gmail.com>
2023-06-30 01:25:10 +0900

    update : ダークモード対応

--- lib/example_card.dart
+++ lib/example_card.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
 import 'example_candidate_model.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:news_app/main.dart';
 
 class ExampleCard extends StatelessWidget {
   final ExampleCandidateModel candidate;
@@ -13,11 +15,12 @@ class ExampleCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final themeNotifier = Provider.of<ThemeNotifier>(context);
+
     return Container(
-      //カードのかげ
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
-        color: CupertinoColors.white,
+        color: themeNotifier.isDarkMode ? Colors.grey.shade800 : Colors.white,
         boxShadow: [
           BoxShadow(
             color: CupertinoColors.systemGrey.withOpacity(0.2),
@@ -30,51 +33,49 @@ class ExampleCard extends StatelessWidget {
       alignment: Alignment.center,
       child: Column(
         children: [
-          Flexible(
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(10),
-                  topRight: Radius.circular(10),
-                ),
-              ),
-            ),
-          ),
           Container(
-            padding: const EdgeInsets.all(15),
-            decoration: const BoxDecoration(
-              color: Colors.white,
+            padding: const EdgeInsets.all(30),
+            decoration: BoxDecoration(
+              //カード本体の色
+              color: themeNotifier.isDarkMode
+                  ? Colors.grey.shade800
+                  : Colors.white,
               borderRadius: BorderRadius.only(
                 bottomLeft: Radius.circular(10),
                 bottomRight: Radius.circular(10),
               ),
             ),
-            child: Row(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      candidate.title!,
-                      style: const TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                      ),
+                Text(
+                  candidate.title!,
+                  style: TextStyle(
+                    color:
+                        themeNotifier.isDarkMode ? Colors.white : Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                InkWell(
+                  onTap: () async {
+                    if (await canLaunch(candidate.link!)) {
+                      await launch(candidate.link!);
+                    }
+                  },
+                  child: Text(
+                    candidate.link!,
+                    style: const TextStyle(
+                      color: Colors
+                          .blue, // Change the color to indicate that it's a link
+                      fontSize: 15,
                     ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    Text(
-                      candidate.link!,
-                      style: const TextStyle(
-                        color: Colors.grey,
-                        fontSize: 15,
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
               ],
             ),
